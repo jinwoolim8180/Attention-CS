57d3c6b52e1df62b7487d0841223b68ba8900714
jinwoolim8180 <jinwoolim8180@snu.ac.kr>
2022-09-28 22:01:36 +0900

    customized

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ from torch.utils.data import Dataset, DataLoader
 import platform
 from argparse import ArgumentParser
 import csdata_fast
-from models import MADUN
+from original import MADUN
 from tqdm import tqdm
 
 parser = ArgumentParser(description='MADUN')
@@ -147,7 +147,7 @@ for epoch_i in range(start_epoch + 1, end_epoch + 1):
         x_output = model(Phix, Phi)
 
         # Compute and print loss
-        loss_all = nn.MSELoss()(x_output, batch_x)
+        loss_all = nn.L1Loss()(x_output, batch_x)
 
         # Zero gradients, perform a backward pass, and update the weights.
         optimizer.zero_grad()
